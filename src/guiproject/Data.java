1a314b5bc1917416b64248b12caf0882556d64ec
Fauzanjaludin <73268282+Fauzanjaludin@users.noreply.github.com>
2021-01-25 21:37:39 +0800

    a

---
+++ src/guiproject/Data.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guiproject;
+
+/**
+ *
+ * @author khairinkasmadi
+ */
+public class Data {
+    
+    private int date;     //2type of data int & string
+    private int accnum;
+    private String name;
+    private String address;
+    private int arrears;
+    private int previous;   //
+    private int current;    //
+    private int totalunit;
+    private int currentcharge;
+    private int totalbill;
+
+    public Data(int date, int accnum, String name, String address, int arrears, int previous, int current, int totalunit, int currentcharge, int totalbill) {
+        this.date = date;     //constructor method
+        this.accnum = accnum;
+        this.name = name;
+        this.address = address;
+        this.arrears = arrears;
+        this.previous = previous;
+        this.current = current;
+        this.totalunit = totalunit;
+        this.currentcharge = currentcharge;
+        this.totalbill = totalbill;
+    }
+
+    Data(int date, int accnum, String name, String address, double arrears, double previous, double current, double totalunit, double currentcharge, double totalbill) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public int getAccnum() {
+        return accnum;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getArrears() {
+        return arrears;
+    }
+
+    public int getPrevious() {
+        return previous;
+    }
+
+    public int getCurrent() {
+        return current;
+    }
+
+    public int getTotalunit() {
+        return totalunit;
+    }
+
+    public int getCurrentcharge() {
+        return currentcharge;
+    }
+
+    public int getTotalbill() {
+        return totalbill;
+    }
+    
+    
+    
+    
+              
+}
---
+++ src/guiproject/UserOperation.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guiproject;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author khairinkasmadi
+ */
+public class UserOperation {
+    
+    public void addRecord(Data usr) throws IOException {
+        //usr = user
+        try{
+        File fileName = new File("user.txt");
+        FileWriter fw = new FileWriter(fileName, true);
+        BufferedWriter bw= new BufferedWriter(fw);
+        PrintWriter pw = new PrintWriter(bw);
+        
+        pw.println(usr.getDate());
+        pw.println(usr.getAccnum());
+        pw.println(usr.getName());
+        pw.println(usr.getAddress());
+        pw.println(usr.getArrears());
+        pw.println(usr.getPrevious());
+        pw.println(usr.getCurrent());
+        pw.println(usr.getTotalunit());
+        pw.println(usr.getCurrentcharge());
+        pw.println(usr.getTotalbill());
+        
+        pw.close();
+        pw.flush();
+        bw.close();
+        fw.close(); 
+        }catch(IOException e){
+            
+        }
+    }
+    
+}
